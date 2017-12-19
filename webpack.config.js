9d12ef0750d28d3a96b8618b307ad3d732efb231
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-19 17:54:02 +0000

    Add React on MongoDB

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
